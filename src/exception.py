c39c0097b0ad0fdc4627db109a844c6f90195e46
HariSudhan76 <g.harisudhan76@gmail.com>
2025-04-15 15:18:10 +0530

    my 3rd commit

---
+++ src/exception.py
@@ -0,0 +1,26 @@
+import sys
+import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exec_tb = error_detail.exc_info() ##exec_tb 3rd variable will give proper info which line number execption has occured etc.
+    file_name = exec_tb.tb_frame.f_code.co_filename
+    error_message = "Error Occured in Python script name [{0}] line number [{1}] error message [{2}]".format(
+            file_name,exec_tb.tb_lineno,str(error)
+    )
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
+

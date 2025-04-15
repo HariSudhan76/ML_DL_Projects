90ab7e59a83f794abc2dc11c8d3f69cc7275759c
HariSudhan76 <g.harisudhan76@gmail.com>
2025-04-15 21:51:04 +0530

    commiting DataIngestion.py

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from logger import logging
 
 def error_message_detail(error,error_detail:sys):
     _,_,exec_tb = error_detail.exc_info() ##exec_tb 3rd variable will give proper info which line number execption has occured etc.
@@ -16,6 +16,14 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Divide by Zero")
+        raise CustomException(e,sys)
+
 
 
 
